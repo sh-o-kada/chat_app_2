f3076cbdd32daccc3476451ec42288d0f87e5a3d
sh-o-kada <shokada.25y@gmail.com>
2022-07-27 18:40:14 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,13 +11,13 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      @messages = @room.messages.include(:user)
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
 
   private
   def message_params
-    params.require(:message).permit(:content).merge(user_id:current_user.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
